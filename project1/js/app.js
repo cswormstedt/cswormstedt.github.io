c40951b074c0b018701d6371d01596d103cd555a
cSwormstedt <cswormstedt7@gmail.com>
2017-02-16 21:17:08 -0600

    collision added

--- project1/js/app.js
+++ project1/js/app.js
@@ -10,6 +10,7 @@ var mouse = [];
 window.onload = function(event){
 
 ball.initBall();
+// ball.initMove();
 animateCanvas();
 
 }
@@ -25,7 +26,7 @@ var ball ={
 
 	position: [],
 
-	direction: '',
+	direction: [],
 
 	// starting position
 	startPoint: function(x, y){
@@ -35,38 +36,50 @@ var ball ={
 		}
 	},
 
-	// movement: function( dx, dy){
+	// puts the staring position
+	initBall: function(){
+        this.position.push(this.startPoint(canvas.width/2, canvas.height -30));
+    },
+
+	//how to put in direction to call in draw function??
+	// movement: function(dx, dy){
 	// 	return{
 	// 		dx: this.position[0].x += dx,
 	// 		dy: this.position[0].y += dy
 	// 	}
 	// },
-	
-	// puts the staring position
-	initBall: function(){
-        this.position.push(this.startPoint(canvas.width/2, canvas.height -30));
-    
-    },
 
- 
+	// //start movement
+	// initMove: function (){
+	// 	this.direction.push(this.movement(2, -2));
+
+	// },
+
 	// draw
 	drawBall: function(){	
 		ctx.beginPath(); 	      
 		ctx.arc((this.position[0].x), (this.position[0].y), ballRadius, 0, 2 * Math.PI);
 		ctx.fillStyle = "rgba(0, 0, 230, 0.7)";
 		ctx.fill();
-		
-
 	},
-
 };
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ball.drawBall();
-    ball.position[0].x += dx;
+    
+	if(ball.position[0].x + dx > canvas.width-ballRadius || ball.position[0].x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(ball.position[0].y + dy > canvas.height-ballRadius || ball.position[0].y + dy < ballRadius) {
+        dy = -dy;
+    }
+
+	ball.position[0].x += dx;
     ball.position[0].y += dy;
-}
+	
+};	
+
 function animateCanvas(){
 	draw();
 	window.requestAnimationFrame(animateCanvas);
