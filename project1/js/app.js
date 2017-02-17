644b3ae78c9e12e551f773602f2f25c096c6d9cb
cSwormstedt <cswormstedt7@gmail.com>
2017-02-16 20:37:20 -0600

    ball is moving

--- project1/js/app.js
+++ project1/js/app.js
@@ -7,14 +7,20 @@ var ctx = canvas.getContext('2d');
 var keys = [];
 var mouse = [];
 
+window.onload = function(event){
 
+ball.initBall();
+animateCanvas();
+
+}
 
 //ball radius to help with wall collision
 var ballRadius = 10;
+var dx = 2;
+var dy = -2;
 
 
-
-//ball
+ball
 var ball ={
 
 	position: [],
@@ -27,25 +33,45 @@ var ball ={
 			x: x,
 			y: y
 		}
-		console.log(hey);
 	},
-	
 
+	// movement: function( dx, dy){
+	// 	return{
+	// 		dx: this.position[0].x += dx,
+	// 		dy: this.position[0].y += dy
+	// 	}
+	// },
+	
+	// puts the staring position
 	initBall: function(){
         this.position.push(this.startPoint(canvas.width/2, canvas.height -30));
     
     },
-    
+
+ 
 	// draw
 	drawBall: function(){	
 		ctx.beginPath(); 	      
 		ctx.arc((this.position[0].x), (this.position[0].y), ballRadius, 0, 2 * Math.PI);
 		ctx.fillStyle = "rgba(0, 0, 230, 0.7)";
 		ctx.fill();
+		
+
 	},
 
 };
 
-// window.requestAnimationFrame(ball.drawBall);
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ball.drawBall();
+    ball.position[0].x += dx;
+    ball.position[0].y += dy;
+}
+function animateCanvas(){
+	draw();
+	window.requestAnimationFrame(animateCanvas);
+};
+
+
 
 
