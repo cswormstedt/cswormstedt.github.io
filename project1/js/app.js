7e4b9d3d80166071b75ec67cbd9db65a98a250b3
cSwormstedt <cswormstedt7@gmail.com>
2017-02-16 19:39:25 -0600

    ball object created and is on canvas

--- project1/js/app.js
+++ project1/js/app.js
@@ -1,1 +1,36 @@
 console.log('yo');
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+//grabbed canvas and created context
+
+var keys = [];
+var mouse = [];
+
+
+
+//ball radius to help with wall collision
+var ballRadius = 10;
+
+
+
+//ball
+var ball ={
+
+	body: [],
+	direction: '',
+	position: '',
+
+
+
+
+	// draw
+	drawBall: function(){	
+		ctx.beginPath(); 	      
+		ctx.arc(30, 30 , ballRadius, 0, 2 * Math.PI);
+		ctx.fillStyle = "rgba 0, 0, 0, 1";
+		ctx.fill();
+	},
+
+};
+
