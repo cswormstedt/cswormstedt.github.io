02288d847b4e4c7f2eeead5ba3c801f452993be0
cSwormstedt <cswormstedt7@gmail.com>
2017-02-17 09:04:35 -0600

    added paddle

--- project1/js/app.js
+++ project1/js/app.js
@@ -10,6 +10,7 @@ var mouse = [];
 window.onload = function(event){
 
 ball.initBall();
+paddle.initPaddle();
 animateCanvas();
 
 }
@@ -17,13 +18,44 @@ animateCanvas();
 //ball radius to help with wall collision
 var ballRadius = 10;
 
+// bricks 
+var brick = {};
+
+//paddle
+var paddle ={
+
+	paddleP: [],
+
+	startPoint: function(x, y){
+		return{
+			x: x,
+			y: y
+		}
+	},
+
+
+	initPaddle: function(){
+        this.paddleP.push(this.startPoint(225, canvas.height -25));
+    },
+
+	drawPaddle: function(){
+		ctx.beginPath();
+		ctx.rect(this.paddleP[0].x, this.paddleP[0].y, 100, 5);
+		ctx.strokeStyle = "rgba(250, 0, 0, 0.9)";
+		ctx.lineWidth = 1;
+		ctx.fillStyle = "rgba(200, 0, 0, 0.7)";
+		ctx.fill();
+		ctx.stroke();
+	},
+
+};
 
 //ball
 var ball ={
 	//position
-	p: [],
+	ballP: [],
 	//direction
-	d: [],
+	ballD: [],
 
 	//position varibles
 	startPoint: function(x, y){
@@ -41,14 +73,14 @@ var ball ={
 	},
 	// sets position and direction varibles
 	initBall: function(){
-        this.p.push(this.startPoint(canvas.width/2, canvas.height -30));
-    	this.d.push(this.movement(2, -2));
+        this.ballP.push(this.startPoint(canvas.width/2, canvas.height -30));
+    	this.ballD.push(this.movement(2, -2));
     },
 
 	// ball is made
 	drawBall: function(){	
 		ctx.beginPath(); 	      
-		ctx.arc((this.p[0].x), (this.p[0].y), ballRadius, 0, 2 * Math.PI);
+		ctx.arc((this.ballP[0].x), (this.ballP[0].y), ballRadius, 0, 2 * Math.PI);
 		ctx.fillStyle = "rgba(200, 0, 0, 0.7)";
 		ctx.fill();
 	},
@@ -57,16 +89,17 @@ var ball ={
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ball.drawBall();
+    paddle.drawPaddle();
     //collison logic
-	if(ball.p[0].x + ball.d[0].dx > canvas.width-ballRadius || ball.p[0].x + ball.d[0].dx < ballRadius) {
-        ball.d[0].dx = -ball.d[0].dx;
+	if(ball.ballP[0].x + ball.ballD[0].dx > canvas.width-ballRadius || ball.ballP[0].x + ball.ballD[0].dx < ballRadius) {
+        ball.ballD[0].dx = -ball.ballD[0].dx;
     }
-    if(ball.p[0].y + ball.d[0].dy > canvas.height-ballRadius || ball.p[0].y + ball.d[0].dy < ballRadius) {
-        ball.d[0].dy = -ball.d[0].dy;
+    if(ball.ballP[0].y + ball.ballD[0].dy > canvas.height-ballRadius || ball.ballP[0].y + ball.ballD[0].dy < ballRadius) {
+        ball.ballD[0].dy = -ball.ballD[0].dy;
     }
     //ball movement
-	ball.p[0].x += ball.d[0].dx;
-    ball.p[0].y += ball.d[0].dy;
+	ball.ballP[0].x += ball.ballD[0].dx;
+    ball.ballP[0].y += ball.ballD[0].dy;
 	
 };	
 
